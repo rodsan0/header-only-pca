d8578585a18ef2cc301d9844af0592b257b0858d
Matthew Andres Moreno <mmore500.login+git@gmail.com>
2020-11-12 13:13:46 -0500

    Free normalization funs, write drop_homogenous

--- include/hopca/pca.hpp
+++ include/hopca/pca.hpp
@@ -6,40 +6,13 @@
 #include <algorithm>
 #include <functional>
 
-
 #include "../../third-party/linalg/single-include/linalg.hpp"
 
-
-#include "types.hpp"
 #include "eigenmap.hpp"
+#include "normalize.hpp"
+#include "types.hpp"
 
 namespace hopca {
-
-inline hopca::Matrix get_covariance(const hopca::Matrix m) {
-    hopca::Matrix ret = hola::matrix_multiply_MtN(m, m);
-
-    std::transform(
-        DATA(ret),
-        DATA(ret) + ret->n_col * ret->n_row,
-        DATA(ret),
-        [n = ret->n_col](const double a) {
-            return a / n;
-        }
-    );
-
-    return ret;
-}
-
-inline double get_mean(const hopca::Vector v) {
-    const size_t len = v->length;
-    double sum = 0;
-
-    for (size_t i = 0; i < len; ++i) {
-        sum += VECTOR_IDX_INTO(v, i);
-    }
-
-    return sum / len;
-}
 
 class PCA {
     size_t n_components;
@@ -68,30 +41,6 @@ public:
     hopca::Matrix doPCA(hopca::Matrix m) const;
 };
 
-hopca::Matrix PCA::normalize(hopca::Matrix m) const {
-    const size_t rows = m->n_row;
-    const size_t cols = m->n_col;
-
-    hopca::Matrix temp = hola::matrix_new(rows, cols);
-    assert(temp != nullptr);
-
-    for (size_t col = 0; col < cols; ++col) {
-        hopca::Vector vec = hola::matrix_column_copy(m, col);
-
-        const hopca::Vector mean_vector
-          = hola::vector_constant(cols, get_mean(vec));
-        hola::vector_subtract_into(vec, vec, mean_vector);
-
-        const double norm_squared = hola::vector_dot_product(vec, vec);
-        const double invstdev = std::sqrt(rows / norm_squared);
-
-        hola::vector_scalar_multiply_into(vec, vec, invstdev);
-
-        hola::matrix_copy_vector_into_column(temp, vec, col);
-    }
-    return temp;
-}
-
 hopca::Matrix PCA::doPCANoNormalize(hopca::Matrix m) const {
 
     // find opposite of matrix
@@ -116,8 +65,7 @@ hopca::Matrix PCA::doPCANoNormalize(hopca::Matrix m) const {
 }
 
 hopca::Matrix PCA::doPCA(hopca::Matrix m) const {
-    const hopca::Matrix normalized = normalize(m);
-    return doPCANoNormalize(normalized);
+    return doPCANoNormalize( hopca::normalize(m) );
 }
 
 } // namespace hopca
